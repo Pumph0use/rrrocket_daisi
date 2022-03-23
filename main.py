a7a4c8e7a8477d6e018f3d9fcb0fca891393d18c
Dustin Sims <dustinsims0@gmail.com>
2022-03-23 12:50:07 -0500

    Try passing file_path

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from rrrocket_processor import process_match_overview
 def process_replay(file_path: pd.DataFrame):
     bin_path = get_latest_rrrocket_release()
 
-    result = subprocess.check_output([bin_path, '-n', r'41805C0A4C99046708C38B9C735919D4.replay'])
+    result = subprocess.check_output([bin_path, '-n', file_path])
     result = json.loads(result.decode('utf-8'))
 
     blue, orange = process_match_overview(result['properties'])
