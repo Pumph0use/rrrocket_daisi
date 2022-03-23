73b95ecc7806120a2819470ebcb3d0d286e55c17
Dustin Sims <dustinsims0@gmail.com>
2022-03-23 12:47:34 -0500

    Type hint black magic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import subprocess
 import json
+
+import pandas as pd
+
 from rrrocket_retriever import get_latest_rrrocket_release
 from rrrocket_processor import process_match_overview
 
 
-def process_replay(file_path: str):
+def process_replay(file_path: pd.DataFrame):
     bin_path = get_latest_rrrocket_release()
 
     result = subprocess.check_output([bin_path, '-n', r'41805C0A4C99046708C38B9C735919D4.replay'])
@@ -14,10 +17,3 @@ def process_replay(file_path: str):
 
     return [{'type': 'dataframe', 'label': 'Blue Team', 'data': blue},
             {'type': 'dataframe', 'label': 'Orange Team', 'data': orange}]
-
-
-# if __name__ == '__main__':
-#     result = subprocess.check_output(['rrrocket.exe', '-n', r'41805C0A4C99046708C38B9C735919D4.replay'])
-#     result = json.loads(result.decode('utf-8'))
-#
-#     blue, orange = process_match_overview(result['properties'])
