11de279009e73cb97bd10a017913f8c93b5a017d
Dustin Sims <dustinsims0@gmail.com>
2022-03-23 12:34:59 -0500

    Fix actually process results

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ def process_replay(file_path: str):
     bin_path = get_latest_rrrocket_release()
 
     result = subprocess.check_output([bin_path, '-n', r'41805C0A4C99046708C38B9C735919D4.replay'])
+    result = json.loads(result.decode('utf-8'))
+
+    blue, orange = process_match_overview(result['properties'])
 
     return [blue, orange]
 
 
-if __name__ == '__main__':
-    result = subprocess.check_output(['rrrocket.exe', '-n', r'41805C0A4C99046708C38B9C735919D4.replay'])
-    result = json.loads(result.decode('utf-8'))
-
-    blue, orange = process_match_overview(result['properties'])
+# if __name__ == '__main__':
+#     result = subprocess.check_output(['rrrocket.exe', '-n', r'41805C0A4C99046708C38B9C735919D4.replay'])
+#     result = json.loads(result.decode('utf-8'))
+#
+#     blue, orange = process_match_overview(result['properties'])
